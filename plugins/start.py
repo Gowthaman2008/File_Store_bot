f3514273c14388f8b09732717219c29bd6d3a632
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-07-29 22:07:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,7 +90,7 @@ async def start_command(client: Client, message: Message):
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ],[ InlineKeyboardButton("Movies Channel ⚡", callback_data = "")
+                ],[ InlineKeyboardButton("Movies Channel ⚡", url="https://t.me/Movies_Tamizhaaas")
                 ]
             ]
         )
