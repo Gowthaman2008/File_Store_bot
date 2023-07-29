408e5c43f9fccdafed114cdc83b673367f68584e
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-07-29 22:43:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Owner : <a href='https://t.me/i_like_surya'>Rohith</a>\n○ Creator : <a href='https://t.me/Gowthaman2008'>Gowthaman</a>\n○ Channel : @Movies_Tamizhaaas</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
+                        InlineKeyboardButton("Movies Channel ⚡", url="https://t.me/Movies_Tamizhaaas")
+                    ],[
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
