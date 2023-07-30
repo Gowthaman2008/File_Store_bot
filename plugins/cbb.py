4336eb5a57cab2f015510555f3e1b54f32a4ffc1
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-07-30 08:40:04 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Owner : <a href='https://t.me/i_like_surya'>Rohith</a>\n○ Creator : <a href='https://t.me/Gowthaman2008'>Gowthaman</a>\n○ Channel : @Movies_Tamizhaaas</b>",
+            text = f"<b>🤖 My Name: <a href='https://t.me/MT_FS_Bot'>MT File Store Bot</a>\n💥 Owner : <a href='https://t.me/i_like_surya'>Rohith</a>\n👨‍💻 Creator : <a href='https://t.me/Gowthaman2008'>Gowthaman</a>\n 📝 Language: <a href='https://www.python.org/'>Python3📚</a>\n 🎈 Library: <a href='https://docs.pyrogram.org/'>Pyrogram 💫</a>\n 🎉 Channel : @Movies_Tamizhaaas</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
